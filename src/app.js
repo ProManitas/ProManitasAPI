ab2b22728aa72baf08eda1a0e7e49ce08d0e389c
Kevin Alfonzo <kalfonzoespinett@gmail.com>
2023-03-29 18:56:25 -0400

    cors

--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
 //IMPORTS 
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const router = require('./routes/index')
 
 
 const server = express();
 
-
-server.use(bodyParser.json())
+server.use(bodyParser.json());
 server.use(morgan('dev'));
+server.use(cors());
 
 server.use('/', router)
 
