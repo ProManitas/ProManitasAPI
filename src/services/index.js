e9dce082cb6b0a50db6bbc73cd640fc5039a62c9
Kevin Alfonzo <kalfonzoespinett@gmail.com>
2023-04-03 20:17:36 -0400

    pagination adposts and users

--- src/services/index.js
+++ src/services/index.js
@@ -7,13 +7,19 @@ const allInf = async (model) => {
     
     switch (model) {
         case 'User':
-            return await User.findAll({where: {deleted: false}, attributes: ['id','username', 'firstname', 'lastname', 'email', 'password', 'cellnumber', 'address', 'image', 'experience']});
+            return await User.findAll({
+                where: {deleted: false}, 
+                attributes: ['id','username', 'firstname', 'lastname', 'email', 'password', 'cellnumber', 'address', 'image', 'experience']
+            });
             
         case 'Services':
             return await Services.findAll();
                 
         case 'Adpost':
-            return await Adpost.findAll({where: {deleted: false}, attributes: ["name", "description", "UserId", "ServiceId"]});
+            return await Adpost.findAll({
+                where: {deleted: false}, 
+                attributes: ["name", "description", "UserId", "ServiceId"]
+            });
     };
 };
 
@@ -24,28 +30,70 @@ const withRole = async (model) => {
 
 //ALL USER WITHOUT ROLE
 const withoutRole = async (model) => {
-    return await User.findAll({where: {role: false, deleted: false}, attributes: ['id','username', 'firstname', 'lastname', 'email', 'password', 'cellnumber', 'address', 'image', 'experience' , 'role']})
+    return await User.findAll({
+        where: {role: false, deleted: false}, 
+        attributes: ['id','username', 'firstname', 'lastname', 'email', 'password', 'cellnumber', 'address', 'image', 'experience' , 'role']
+    });
 };
+
+//PAGINATION
+const pagination = async (model, pageNumber, pageSize) => {
+    const limit = parseInt(pageSize);
+    const offset = (parseInt(pageNumber) - 1) * limit;
+
+    switch (model) {
+        case 'User':
+            
+            return await User.findAll({
+            limit,
+            offset,
+            where: {deleted: false},
+            attributes: ['id','username', 'firstname', 'lastname', 'email', 'password', 'cellnumber', 'address', 'image', 'experience'],
+            order: [['username', 'ASC']],
+            });
+            
+        case 'Adpost' :
+            
+            return await Adpost.findAll({
+            limit,
+            offset,
+            where: {deleted: false},
+            attributes: ["name", "description", "UserId", "ServiceId"],
+            order: [['name', 'ASC']],
+            });
+            
+        default:
+            break;
+    };
+};
+
 
 //FILTER BY ID
 const filterID = async (model , id) => {
     switch (model) {
         case 'User':
-            return await User.findByPk(id, {where: { deleted: false }, attributes: ['username', 'firstname', 'lastname', 'email', 'password', 'cellnumber', 'address', 'image', 'experience']});
+            return await User.findByPk(id, {
+                where: { deleted: false }, 
+                attributes: ['username', 'firstname', 'lastname', 'email', 'password', 'cellnumber', 'address', 'image', 'experience']
+            });
 
         case 'Services':
             return await Services.findByPk(id);    
-    };
+        };
 };
 
 //FILTER BY NAME 
 const filterName = async (model, name) => {
     switch (model) {
         case 'User':
-            return await User.findAll({where: { firstname: { [Op.like]: `%${name}%`}}});
+            return await User.findAll({
+                where: { firstname: { [Op.like]: `%${name}%`}}
+            });
             
         case 'Adpost':
-            return await Adpost.findAll({where: { name: { [Op.like]: `%${name}%`}}});
+            return await Adpost.findAll({
+                where: { name: { [Op.like]: `%${name}%`}}
+            });
     };
 };
 
@@ -55,6 +103,7 @@ module.exports = {
     allInf,
     withRole,
     withoutRole,
+    pagination,
     filterID,
     filterName
 }
