25cea53c7dc9396018d1805f9c61fc9aa226a2ef
AlfonzoDev <104286335+RaiderAlf@users.noreply.github.com>
2023-04-04 21:18:27 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const server = express();
 server.use(bodyParser.json());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
   next();
