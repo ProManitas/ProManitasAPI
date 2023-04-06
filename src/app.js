3a89e2ce8c34f21808b05d9b3bd7f0de1829edfd
dessctras <dessctras@gmail.com>
2023-04-06 01:37:57 -0300

    login

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const router = require('./routes/index')
+const router = require('./routes/index');
+const { verifyJwToken } = require('./login/login');
 
 
 const server = express();
@@ -16,6 +17,29 @@ server.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
   next();
 });
+
+//SE NECESITAN GET DE RUTAS PROTEGIDAS DEL FRONT
+server.use(verifyJwToken)
+
+server.get('/protected', async(req, res)=>{
+  try {
+    const accesToken = req.headers.authorization.split(' ')[1];
+    const response = await axios.get('misma url issuer con otra ruta con la info del usuario', {
+      headers:{
+        authorization: `viene del front ${accesToken}`
+      }
+    })
+    const userInfo = response.data
+    console.log(userInfo);
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+server.use((err, req, res, next)=>{
+  const status = err.status || 500
+  const message = err.message || 'Error interno en el servidor'
+})
 
 server.use('/', router)
 
