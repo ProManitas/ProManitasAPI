56dc09450dcc1e09db2528f05c0c40fdf39e1305
Kevin Alfonzo <kalfonzoespinett@gmail.com>
2023-04-18 16:23:25 -0400

    NotDB

--- index.js
+++ index.js
@@ -22,55 +22,55 @@
 //------------------------MOCK DATA----------------------------------
 const { User, Services, Adpost, Rating, Contract } = require('./src/db');
 
-const fakeDb = require('./src/MOCK_DATA_SERVICES.json');
-const fakeDbUsers = require('./src/MOCK_DATA_USERS.json');
-const fakeDbAdpost = require('./src/MOCK_DATA_ADPOST.json');
-const fakeDbContracts = require('./src/MOCK_DATA_CONTRACTS.json')
+// const fakeDb = require('./src/MOCK_DATA_SERVICES.json');
+// const fakeDbUsers = require('./src/MOCK_DATA_USERS.json');
+// const fakeDbAdpost = require('./src/MOCK_DATA_ADPOST.json');
+// const fakeDbContracts = require('./src/MOCK_DATA_CONTRACTS.json')
 
-const filledDbServices = async () =>{ 
-    fakeDb.map(e => Services.create({name : e.name, image:e.image }));
-};
+// const filledDbServices = async () =>{ 
+//     fakeDb.map(e => Services.create({name : e.name, image:e.image }));
+// };
 
-const filledDbAdpost = async () =>{
-    fakeDbAdpost.map(post => Adpost.create({name : post.name, description: post.description, image: post.image}));
-};
+// const filledDbAdpost = async () =>{
+//     fakeDbAdpost.map(post => Adpost.create({name : post.name, description: post.description, image: post.image}));
+// };
 
-const filledContracts = async () =>{    
-    fakeDbContracts.map(post => Contract.create({
-        commencementDate: post.commencementDate, 
-        terminationDate: post.terminationDate,
-        payment: post.payment
-    }))
-}
+// const filledContracts = async () =>{    
+//     fakeDbContracts.map(post => Contract.create({
+//         commencementDate: post.commencementDate, 
+//         terminationDate: post.terminationDate,
+//         payment: post.payment
+//     }))
+// }
 
-const filledDbUsers = async () =>{
-    fakeDbUsers.map(e => User.create({
-        username: e.username,
-        firstname: e.firstname,
-        lastname: e.lastname, 
-        email: e.email,
-        password: e.password,
-        cellnumber: e.cellnumber,
-        address: e.address,
-        role: e.role,
-        experience: e.experience,
-        image: e.image,
-        delete: e.delete
-    }));
-};
+// const filledDbUsers = async () =>{
+//     fakeDbUsers.map(e => User.create({
+//         username: e.username,
+//         firstname: e.firstname,
+//         lastname: e.lastname, 
+//         email: e.email,
+//         password: e.password,
+//         cellnumber: e.cellnumber,
+//         address: e.address,
+//         role: e.role,
+//         experience: e.experience,
+//         image: e.image,
+//         delete: e.delete
+//     }));
+// };
 
 
 const server = require('./src/app.js');
 const { database } = require('./src/db');
 
 // SYNC MODELS
-database.sync({ force: true })
+database.sync({ alter: true })
     .then(() => {
         server.listen(3001, () => {
-            filledDbUsers() 
-            filledDbServices()
-            filledDbAdpost()
-            filledContracts()
+            // filledDbUsers() 
+            // filledDbServices()
+            // filledDbAdpost()
+            // filledContracts()
             console.log('Listening at' + ' ' + 3001)
         });
     });
